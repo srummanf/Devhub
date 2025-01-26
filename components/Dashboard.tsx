0f121f481b7380409d6a454a14b4304b400e6bbb
Rumman <rummanfardeen4567@gmail.com>
2025-01-26 13:05:13 +0530

    Cards rebuilt

--- components/Dashboard.tsx
+++ components/Dashboard.tsx
@@ -1,21 +1,30 @@
 "use client";
 import React from "react";
 import { Card } from "@/components/ui/Card";
+import { PinContainer } from "@/components/ui/3d-pin";
+
 import { websites } from "../data/website";
 
 export function Dashboard() {
   return (
     <div className="container mx-auto px-4 py-8">
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
-        {websites.map((site, index) => (
-          <Card
-            key={index}
-            title={site.title}
-            href={site.href}
-            imageUrl={site.imageUrl}
-            description={site.description}
-          />
-        ))}
+        <PinContainer
+          title="/ui.aceternity.com"
+          href="https://twitter.com/mannupaaji"
+        >
+          <div className="flex basis-full flex-col p-4 tracking-tight text-slate-100/50 sm:basis-1/2 w-[20rem] h-[20rem] ">
+            <h3 className="max-w-xs !pb-2 !m-0 font-bold  text-base text-slate-100">
+              Aceternity UI
+            </h3>
+            <div className="text-base !m-0 !p-0 font-normal">
+              <span className="text-slate-500 ">
+                Customizable Tailwind CSS and Framer Motion Components.
+              </span>
+            </div>
+            <div className="flex flex-1 w-full rounded-lg mt-4 bg-gradient-to-br from-violet-500 via-purple-500 to-blue-500" />
+          </div>
+        </PinContainer>
       </div>
     </div>
   );
